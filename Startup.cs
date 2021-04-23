84005f4027be75c06ee012618071ca6979b8303e
vghanvat007 <83055122+vghanvat007@users.noreply.github.com>
2021-04-23 15:42:07 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging; 
 
 namespace sampleProject
 {
